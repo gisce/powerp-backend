29a76a4814093ae8c6d1b0b38f42f86dcbaa242f
Xavier Barnada <xbarnada@gisce.net>
2016-09-08 17:31:22 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+with open('requirements.txt') as f:
+    data = f.read()
+reqs = data.split()
+
+setup(
+    name='backend',
+    version='0.0.1',
+    packages=find_packages(),
+    url='http://git.gisce.net/gis/backend',
+    license='MIT',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    description='Bakcend service to connect to OpeERP',
+    entry_points='''
+        [console_scripts]
+        backend=backend.app
+    ''',
+    install_requires=reqs
+)
