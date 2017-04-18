7e2cb8bb44f960537b76d1c370cb856b7ea707a1
Eduard Carreras <ecarreras@gisce.net>
2017-04-18 19:03:16 +0200

    Bump to v0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ reqs = data.split()
 
 setup(
     name='backend',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     url='http://git.gisce.net/gis/backend',
     license='MIT',
