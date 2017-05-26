2b6e623ab81c3ac73989519663726fc6bafb6d19
Eduard Carreras <ecarreras@gisce.net>
2017-05-26 12:01:07 +0200

    Update to new Cerberus API

--- backend/validators.py
+++ backend/validators.py
@@ -1,27 +1,29 @@
 from datetime import datetime
 
-from cerberus import Validator, errors
+from cerberus import Validator
 
 
 class OpenERPValidator(Validator):
 
-    def validate(self, document, schema=None, update=False, context=None):
+    def validate(self, document, schema=None, update=False, normalize=True):
         if 'id' in document:
             update = True
         else:
             update = False
         return super(OpenERPValidator, self).validate(
-            document, schema=schema, update=update, context=context
+            document, schema=schema, update=update, normalize=normalize
         )
 
-    def _validate_type_datetime_str(self, field, value):
+    def _validate_type_datetime_str(self, value):
         try:
             datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+            return True
         except ValueError:
-            self._error(field, errors.ERROR_BAD_TYPE.format('datetime_str'))
+            pass
 
-    def _validate_type_date_str(self, field, value):
+    def _validate_type_date_str(self, value):
         try:
             datetime.strptime(value, '%Y-%m-%d')
+            return True
         except ValueError:
-            self._error(field, errors.ERROR_BAD_TYPE.format("date_str"))
+            pass
