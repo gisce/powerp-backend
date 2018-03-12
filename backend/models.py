ad7cd3fac1ae666bee46db593179452352223dac
Xevi <xbarnada@gmail.com>
2018-03-12 19:18:13 +0100

    Use read with domain

--- backend/models.py
+++ backend/models.py
@@ -192,7 +192,6 @@ class ModelBunch(BaseResource):
             search_params = []
         try:
             count = model.search_count(search_params)
-            res_ids = model.search(search_params, limit=limit, offset=offset)
         except Fault:
             response = jsonify({'status': 'ERROR'})
             response.status_code = 422
@@ -202,10 +201,8 @@ class ModelBunch(BaseResource):
         else:
             schema = list(model.fields_get().keys())
         schema = unflatdot(schema)
-        items = []
-        for item_id in res_ids:
-            values = model.read(item_id, list(schema.keys()))
-            items.append(normalize(model, values, schema))
+        fields = list(schema.keys())
+        items = model.read(search_params, fields=fields, limit=limit, offset=offset)
         return jsonify({
             'items': items, 'n_items': count, 'limit': limit, 'offset': offset
         })
