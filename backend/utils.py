a304bdfb674757024bfce32f794a091da0f6c5ba
Eduard Carreras <ecarreras@gisce.net>
2018-06-01 09:51:04 +0200

    Fix checking state with default states

--- backend/utils.py
+++ backend/utils.py
@@ -204,8 +204,11 @@ def make_schema(model, fields):
             'readonly': bool(attrs.get('readonly', False))
         }
         states = attrs.get('states', {})
-        if states and 'state' in data:
-            schema[field].update(dict(states[data['state']]))
+        if states:
+            if 'state' in data:
+                schema[field].update(dict(states[data['state']]))
+            elif 'state' in defaults_fields:
+                schema[field].update(dict(states[defaults_fields['state']]))
 
         type_ = attrs['type']
         if type_ in ('text', 'char', 'selection', 'binary'):
